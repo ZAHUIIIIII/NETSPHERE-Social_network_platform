0521f3fdaf18be73605b9bad0c4d55d85f9880eb
Zahuii <leeminhuy47@gmail.com>
2025-11-13 19:28:39 +0700

    remove strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -20,9 +19,7 @@ if (savedTheme) {
 }
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 )
